fixing hessians

python/dyno_model/neural_network_predictor.py
@@ -130,7 +130,8 @@ class NeuralNetworkPredictor(DynamicModel):
         signal = self.measure(u)
         u = np.reshape(u[1:], (1, -1))
         u = np.concatenate((signal, u), axis = 1)
-        jaco= np.sum(self.__jacobian_tensorflow(u), axis = 0)
+        jaco= np.sum(self.__jacobian_tensorflow(u), axis = 1)
+        print(jaco)
         return jaco
 
     def Ju(self, u):
@@ -140,17 +141,18 @@ class NeuralNetworkPredictor(DynamicModel):
         u = np.reshape(u[1:], (1, -1))
         u = np.concatenate((signal, u), axis = 1)
 
+        hess =[]
         for m in range(self.output_size):
             grad_func = tf.gradients(self.model.output[:, m], self.model.input)
             gradients = sess.run(grad_func, feed_dict={self.model.input: u.reshape((1, u.size))})
             grad = gradients[0][0, :]
-
-            hess=[]
-            for i in range(len(grad)):
-                hess += [self.__jacobian_tensorflow(grad)]
+            hess += [self.__jacobian_tensorflow(grad)]
 
         hess = np.array(hess)
-        hess = np.sum(hess, axis = 1)
+
+        hess = np.sum(hess, axis = 0)
+
+        hess = np.transpose(hess)
 
         self.Hessian = hess
         return self.Hessian

python/example/test_controller.py
@@ -13,7 +13,7 @@ NNP = NeuralNetworkPredictor(model_file = filename, N1 = 5, N2= 10, Nu = 3, ym =
 NR_opt = NewtonRaphson(cost= NNP.Cost, d_model= NNP)
 
 new_state_new = np.random.multivariate_normal([0.05]*12, 1.5*np.eye(12), 1)
-du = [.5]*3
+du = [.3]*3
 
 for n in range(1000):
 
@@ -24,7 +24,7 @@ for n in range(1000):
     new_state_old = new_state_new
 
     u_optimal = np.reshape(NR_opt.optimize(n, du, future_outputs, False)[0], (-1, 1))
-    print(u_optimal)
+    print(new_state_old[:, -3:])
     du = np.array(new_state_old[:, -3:]) - np.array(u_optimal.flatten())
 
     du = du.flatten()