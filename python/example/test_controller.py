0c8801946b680c09b5694e6d42a2513b92376a73
sarahaguasvivas <saag5228@colorado.edu>
2019-09-17 09:56:46 -0600

    plotting simulation

--- python/example/test_controller.py
+++ python/example/test_controller.py
@@ -16,7 +16,11 @@ NR_opt = NewtonRaphson(cost= NNP.Cost, d_model= NNP)
 
 new_state_new = np.random.multivariate_normal([0.05]*12, 1.5*np.eye(12), 1)
 du = [0.0]*3
+
 elapsed=[]
+u_optimal_list=[]
+ym = []
+state = []
 for n in range(10):
 
     seconds = time.time()
@@ -24,12 +28,14 @@ for n in range(10):
     future_outputs = NNP.predict(new_state_new)
 
     NNP.yn = future_outputs.flatten()
-    print(NNP.yn)
+
     future_outputs = future_outputs.flatten()
 
     new_state_old = new_state_new
 
-    u_optimal = np.reshape(NR_opt.optimize(n, du, future_outputs, True)[0], (-1, 1))
+    u_optimal = np.reshape(NR_opt.optimize(n, du, future_outputs, False)[0], (-1, 1))
+
+    u_optimal_list+=[u_optimal.flatten().tolist()]
 
     du = np.array(new_state_old[:, -3:].flatten()) - np.array(u_optimal.flatten())
 
@@ -42,11 +48,44 @@ for n in range(10):
     signals = NNP.measure(u_optimal[:, :, 0])
 
     new_state_new = np.concatenate((signals, np.reshape(u_optimal.flatten()[1:], (1, -1))), axis=1)
+    print(new_state_new.shape)
+    elapsed += [time.time() - seconds]
 
-    elapsed = [time.time() - seconds]
+    print("yn: ", NNP.yn, " time elapsed: ", elapsed[-1], " [s]")
+    ym += [NNP.ym]
+    state += [new_state_new[:, -3:]]
 
 
 
+elapsed = np.array(elapsed).flatten()
+ym = np.reshape(ym, (-1, 3))
+state = np.reshape(state, (-1, 3))
+u_optimal_list = np.reshape(u_optimal_list, (-1, 3))
+
+plt.subplot(4, 1, 1)
+plt.plot(elapsed)
+
+plt.subplot(4, 1, 2)
+plt.plot(ym[:, 0], '--k', label= 'target')
+plt.plot(state[:, 0], 'k', label = 'state')
+plt.plot(u_optimal_list[:, 0], 'r', label = 'control input')
+plt.legend()
+plt.ylabel("block distance")
+
+plt.subplot(4, 1, 3)
+plt.plot(ym[:, 1], '--k', label='target')
+plt.plot(state[:, 1], 'k', label = 'state')
+plt.plot(u_optimal_list[:, 1], 'r', label = 'control_input')
+plt.ylabel("twist sine")
+
+plt.subplot(4, 1, 4)
+plt.plot(ym[:, 2], '--k', label = 'target')
+plt.plot(state[:, 2], 'k', label = 'state')
+plt.plot(u_optimal_list[:, 2], 'r', label = "control_input")
+plt.ylabel('twist cosine')
+plt.legend()
+
+plt.show()
 
 
 
