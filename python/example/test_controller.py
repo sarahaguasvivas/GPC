d78105c605ff47490afad6a113a10ea5dc293a62
sarahaguasvivas <saag5228@colorado.edu>
2019-09-25 12:16:07 -0600

    fixed angle estimations

--- python/example/test_controller.py
+++ python/example/test_controller.py
@@ -11,12 +11,10 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 filename= "../model_data/neural_network_2.hdf5"
 
-NNP = NeuralNetworkPredictor(model_file = filename, N1 = 0, N2= 2, Nu = 3, ym = [10., 0.0], K = 5, yn = [0.]*2, lambd = [1, 5.0, 5.0])
+NNP = NeuralNetworkPredictor(model_file = filename, N1 = 0, N2= 2, Nu = 3, ym = [10., 0.5], K = 5, yn = [0.]*2, lambd = [1, 5.0, 5.0])
 NR_opt = NewtonRaphson(cost= NNP.Cost, d_model= NNP)
 
 new_state_new = np.random.multivariate_normal([0.0]*12, 1.5*np.eye(12), 1)
-new_state_new[:, -1] = 0.0
-new_state_new[:, -2] = 1.0
 
 du = [0.0]*3
 
@@ -24,6 +22,7 @@ elapsed=[]
 u_optimal_list=[]
 ym = []
 state = []
+yn = []
 
 for n in range(100):
     seconds = time.time()
@@ -31,8 +30,15 @@ for n in range(100):
     future_outputs = NNP.predict(new_state_new).flatten()
 
     NNP.yn[0] = future_outputs[0]
+    NNP.yn[1] = np.arctan2(future_outputs[-2], future_outputs[-1])
+    NNP.yn[1] = (NNP.yn[1]) % np.pi
 
-    NNP.yn[1] = np.arctan2(future_outputs[1], future_outputs[2])
+    if np.sin(0.5*n) > 0:
+        NNP.ym[0] = 1
+        NNP.ym[1] = 1
+    else:
+        NNP.ym[0] = 0
+        NNP.ym[1] = 0
 
     new_state_old = new_state_new
 
@@ -54,28 +60,32 @@ for n in range(100):
 
     elapsed += [time.time() - seconds]
 
-    print("yn: ", NNP.yn, " time elapsed: ", elapsed[-1], " [s]")
-    ym += [NNP.ym]
+    print("yn: ", NNP.yn,"ym: ", NNP.ym, " time elapsed: ", elapsed[-1], " [s]")
+
+    ym += [NNP.ym[0],NNP.ym[1]]
+    yn += [NNP.yn[0],NNP.yn[1]]
+
     state += [new_state_new[:, -3], np.arctan2(new_state_new[:, -2], new_state_new[:, -1])]
 
 elapsed = np.array(elapsed).flatten()
 ym = np.reshape(ym, (-1, 2))
 state = np.reshape(state, (-1, 2))
 u_optimal_list = np.reshape(u_optimal_list, (-1, 3))
+yn = np.reshape(yn, (-1, 2))
 
 plt.subplot(3, 1, 1)
 plt.plot(elapsed)
 
 plt.subplot(3, 1, 2)
 plt.plot(ym[:, 0], '--k', label= 'target')
-plt.plot(state[:, 0], 'r', label = 'state')
+plt.plot(yn[:, 0], 'r', label = 'state')
 
 plt.legend()
 plt.ylabel("block distance")
 
 plt.subplot(3, 1, 3)
 plt.plot(ym[:, 1], '--k', label='target')
-plt.plot(state[:, 1], 'r', label = 'state')
+plt.plot(yn[:, 1], 'r', label = 'state')
 plt.ylabel("twist")
 
 plt.show()
