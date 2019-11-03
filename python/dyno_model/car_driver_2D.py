cffe3c2a825d38d36643da1e54a4f7b640aab987
sarahaguasvivas <sarahaguasvivas@Sarahs-MacBook-Pro.local>
2019-11-03 09:50:34 -0700

    made par_eps_par_u independent of solver

--- python/dyno_model/car_driver_2D.py
+++ python/dyno_model/car_driver_2D.py
@@ -5,6 +5,7 @@ from constraints.constraints import *
 import numpy as np
 import math
 from scipy.integrate import odeint
+import copy
 
 class Driver2D(DynamicModel):
     def __init__(self, ym : list, K : int, \
@@ -26,6 +27,8 @@ class Driver2D(DynamicModel):
         self.mass: float = 1587  # Toyota Camry gross mass (kg)
 
         self.corn_stiff = 35000 #magnus paper
+
+        self.par_eps_par_u =  np.zeros((6, 2)) # partial epsilon partial u
 
         self.Fcr = 0.
         self.Fcf= 0.
@@ -69,17 +72,17 @@ class Driver2D(DynamicModel):
 
         self.__update_cornering_forces(self.state, u)
 
-        x_6 = np.reshape(self.state[6:], (-1, 2))
-        return np.dot(C, x_6)
+        return np.dot(C, self.par_eps_par_u)
 
     def Ju(self, u, del_u):
         return self.compute_jacobian(u, del_u)
 
     def Fu(self, u, del_u):
-        future_state = self.predict(u, del_u, self.K).tolist()
+        future_state, _ = self.predict(u, del_u, self.K)
+        future_state = future_state.tolist()
         fu = [0.0, 0.0]
-        fu[0] = self.ym[0] - self.yn[0]
-        temp = self.ym[1] - self.yn[1]
+        fu[0] = self.ym[0] - future_state[0]
+        temp = self.ym[1] - future_state[1]
         fu[1] = (temp + np.pi) % (2.*np.pi) - np.pi
         return np.array(fu)
 
@@ -98,17 +101,18 @@ class Driver2D(DynamicModel):
             u[0] : acceleration
             u[1] : steering
         """
-        old_state = self.state
+        old_state = copy.copy(self.state)
         steps = np.arange(0.0, T, T / 10.0) # We're predicting after T steps
         delta_ode_state = odeint(self.__integrator, old_state, steps, args= (u,del_u))
-        return delta_ode_state[-1]
+        delta_ode_parepsparu = odeint(self.__integrator_2, self.par_eps_par_u.flatten(), steps, args=(u, del_u, delta_ode_state[-1]))
+        return delta_ode_state[-1], np.reshape(delta_ode_parepsparu[-1], (-1, 2))
 
     def compute_cost(self, u, del_u):
         return self.Cost.compute_cost(u, del_u)
 
     def __update_cornering_forces(self, state, u):
         acceleration, steering = u
-        x_0, x_1, x_2, x_3, x_4, x_5 = state[:6]
+        x_0, x_1, x_2, x_3, x_4, x_5 = state
         eps = 1e-8
 
         self.Fcf = self.corn_stiff * (steering - np.arctan((x_3 + self.lf*x_5) / (x_2 + eps)))
@@ -116,7 +120,7 @@ class Driver2D(DynamicModel):
 
     def _partial_f_partial_xi(self, state, u):
         acceleration, steering = u
-        x_0, x_1, x_2, x_3, x_4, x_5 = state[:6]
+        x_0, x_1, x_2, x_3, x_4, x_5 = state
         eps = 1e-8
         """
             [ 0, 0, cos(x4),  -sin(x4), - x3*cos(x4) - x2*sin(x4),0]
@@ -127,7 +131,10 @@ class Driver2D(DynamicModel):
             [ 0, 0, -(2*((C*lr*(x3 - lr*x5))/(x2^2*((x3 - lr*x5)^2/x2^2 + 1)) - (C*lf*cos(u1)*(x3 + lr*x5))/(x2^2*((x3 + lr*x5)^2/x2^2 + 1))))/Iz, (2*((C*lr)/(x2*((x3 - lr*x5)^2/x2^2 + 1)) - (C*lf*cos(u1))/(x2*((x3 + lr*x5)^2/x2^2 + 1))))/Iz, 0, -(2*((C*lr^2)/(x2*((x3 - lr*x5)^2/x2^2 + 1)) + (C*lf*lr*cos(u1))/(x2*((x3 + lr*x5)^2/x2^2 + 1))))/Iz]
 
         """
-
+        if x_2 == 0:
+            x_2+= eps
+        if x_5 == 0:
+            x_5 +=eps
         return np.array([[0, 0, np.cos(x_4), -np.sin(x_4), -x_3*np.cos(x_4) - x_2*np.sin(x_4), 0],
                 [0, 0, np.sin(x_4), np.cos(x_4), x_2*np.cos(x_4) - x_3*np.sin(x_4), 0],
                 [0, 0, 0, x_5, 0, x_3],
@@ -135,20 +142,20 @@ class Driver2D(DynamicModel):
                                     self.lr * x_5)/(eps+(x_3+self.lf*x_5)**2 + x_2**2)) + \
                                     self.corn_stiff*((x_3-self.lr*x_5)/(eps+(x_3-self.lr*x_5)**2 + x_2**2))), \
 
-                                     -(2.*(self.corn_stiff/(x_2*((x_3 - self.lr*x_5)**2/x_2**2 + 1.)) + \
-                                    (self.corn_stiff*np.cos(steering))/(x_2*((x_3 + \
-                                    self.lf*x_5)**2/x_2**2 + 1))))/self.mass, \
+                                     -(2.*(self.corn_stiff/((x_2+eps)*((x_3 - self.lr*x_5)**2/(x_2**2+eps) + 1.)) + \
+                                    (self.corn_stiff*np.cos(steering))/(eps + x_2*((x_3 + \
+                                    self.lf*x_5)**2/(x_2**2+eps) + 1))))/self.mass, \
                                     0, \
-                                    -(2.*(self.corn_stiff/(x_2*((x_3 - self.lr*x_5)**2/x_2**2 + 1)) +\
-                                    (self.corn_stiff*np.cos(steering))/(x_2*((x_3 + self.lf*x_5)**2/x_2**2 + 1))))/self.mass],
+                                    -(2.*(self.corn_stiff/((x_2+eps)*((x_3 - self.lr*x_5)**2/(x_2+eps)**2 + 1)) +\
+                                    (self.corn_stiff*np.cos(steering))/((x_2+eps)*((x_3 + self.lf*x_5)**2/(x_2**2+eps) + 1))))/self.mass],
                 [0, 0, 0, 0, 0, 1],
-                [ 0, 0, -(2.*((self.corn_stiff*self.lr*(x_3 - self.lr*x_5))/(x_2**2*((x_3 - self.lr*x_5)**2/x_2**2 + 1.)) -\
-                                (self.corn_stiff*self.lf*np.cos(steering)*(x_3 + self.lr*x_5))/(x_2**2*((x_3 + self.lr*x_5)**2/x_2**2 + 1))))/self.yaw_inertia, \
-                                        (2.*((self.corn_stiff*self.lr)/(x_2*((x_3 - self.lr*x_5)**2/x_2**2 + 1)) - \
-                                        (self.corn_stiff*self.lf*np.cos(steering))/(x_2*((x_3 + self.lr*x_5)**2/x_2**2 + \
+                [ 0, 0, -(2.*((self.corn_stiff*self.lr*(x_3 - self.lr*x_5))/((x_2+eps)**2*((x_3 - self.lr*x_5)**2/(x_2**2+eps) + 1.)) -\
+                                (self.corn_stiff*self.lf*np.cos(steering)*(x_3 + self.lr*x_5))/((x_2+eps)**2*((x_3 + self.lr*x_5)**2/(x_2**2+eps) + 1))))/self.yaw_inertia, \
+                                        (2.*((self.corn_stiff*self.lr)/((x_2+eps)*((x_3 - self.lr*x_5)**2/(x_2**2+eps) + 1)) - \
+                                        (self.corn_stiff*self.lf*np.cos(steering))/(eps + x_2*((x_3 + self.lr*x_5)**2/(x_2+eps)**2 + \
                                         1))))/self.yaw_inertia, 0, -(2.*((self.corn_stiff*self.lr**2)/(x_2*((x_3 - \
-                                        self.lr*x_5)**2/x_2**2 + 1)) + (self.corn_stiff*self.lf*self.lr*np.cos(steering))/(x_2*((x_3 +
-                                        self.lr*x_5)**2/x_2**2 + 1))))/self.yaw_inertia]])
+                                        self.lr*x_5)**2/(x_2+eps)**2 + 1)) + (self.corn_stiff*self.lf*self.lr*np.cos(steering))/(x_2*((x_3 +
+                                        self.lr*x_5)**2/(x_2+eps)**2 + 1))))/self.yaw_inertia]])
 
     def _partial_f_partial_u(self, state, u):
         """
@@ -160,18 +167,29 @@ class Driver2D(DynamicModel):
             [ 0, (2*(C*lf*cos(u1) - C*lf*sin(u1)*(u1 - atan((x3 + lr*x5)/x2))))/Iz]
         """
         acceleration, steering = u
-        x_0, x_1, x_2, x_3, x_4, x_5 = state[:6]
+        x_0, x_1, x_2, x_3, x_4, x_5 = state
         eps= 1e-8
         return np.array([[0, 0],
                         [0, 0],
                         [1, 0],
                         [0, (2.*(self.corn_stiff*np.cos(steering) - \
                                 self.corn_stiff*np.sin(steering)*(steering -\
-                                np.arctan((x_3 + self.lf*x_5)/x_2))))/self.mass],
+                                np.arctan((x_3 + self.lf*x_5)/(x_2+eps)))))/self.mass],
                         [0, 0],
                         [0, (2.*(self.corn_stiff*self.lf*np.cos(steering) - \
                                 self.corn_stiff*self.lf*np.sin(steering)*(steering - \
-                                np.arctan((x_3 + self.lr*x_5)/x_2))))/self.yaw_inertia]])
+                                np.arctan((x_3 + self.lr*x_5)/(x_2+eps)))))/self.yaw_inertia]])
+
+    def __integrator_2(self, par_eps_par_u, t, u, del_u, state):
+        """
+        This is the integrator to calculate equation (24)
+
+        """
+        self.__update_cornering_forces(state, u)
+        par_eps_par_u = np.reshape(par_eps_par_u, (-1, 2))
+        x_6_dot = np.dot(self._partial_f_partial_xi(state, u) ,  par_eps_par_u) + self._partial_f_partial_u(state, u)
+
+        return x_6_dot.flatten()
 
     def __integrator(self, state, t, u, del_u):
         """
@@ -190,8 +208,7 @@ class Driver2D(DynamicModel):
         """
         acceleration, steering_angle = u
 
-        x_0, x_1, x_2, x_3, x_4, x_5 = state[:6]
-        x_6 = state[6:]
+        x_0, x_1, x_2, x_3, x_4, x_5 = state
 
         self.__update_cornering_forces(state, u)
         x_2 = max(0, x_2)
@@ -202,15 +219,11 @@ class Driver2D(DynamicModel):
         x_4_dot =  x_5
         x_5_dot = 2./self.yaw_inertia * (self.lf * self.Fcf * np.cos(steering_angle) - self.lr*self.Fcr)
 
-        x_6 = np.reshape(x_6, (-1, 2))
-        x_6_dot = np.dot(self._partial_f_partial_xi(self.state, u) ,  x_6) + self._partial_f_partial_u(self.state, u)
-
         dFdt =  [x_0_dot, x_1_dot, x_2_dot, x_3_dot, x_4_dot, x_5_dot]
-        dFdt += x_6_dot.flatten().tolist()
 
         return dFdt
 
     def future_outputs(self, u, del_u):
-        future_state = self.predict(u, del_u, self.K).tolist()
-        return future_state[:2]
+        future_state, _ = self.predict(u, del_u, self.K)
+        return future_state.tolist()[:2]
 
--- python/example/car_driver.py
+++ python/example/car_driver.py
@@ -4,22 +4,23 @@ from optimizer.newton_raphson import *
 import matplotlib.pyplot as plt
 
 ############### TUNING PARAMS: ##########################
-MAX_ACCEL= 10.
-MIN_ACCEL = -0.5
+MAX_ACCEL= 40.
+MIN_ACCEL = -40
 MAX_STEERING = np.pi / 4.0
-R = 10.
+R = 100.
 SIM_STEP = 0.1
-MAX_SIM_STEPS = 100
+MAX_SIM_STEPS = 10
 MAX_NR_IT = 8 # Maximum Newton Raphson Iterations
+ALPHA= 30.
 #########################################################
 
-D2D = Driver2D(ym = [0.0, 0.0], K = 0.5, yn = [0.0, 0.0], alpha = 1.)
+D2D = Driver2D(ym = [0.0, 0.0], K = 0.5, yn = [0.0, 0.0], alpha = 30.)
 D2D_opt = NewtonRaphson(cost= D2D.Cost, d_model= D2D)
 
 del_u = np.array([0.0, 0.0])
-u_optimal = np.array([MAX_ACCEL, 0.0])
+u_optimal = np.array([MAX_ACCEL, np.pi/4.0])
 
-state_new = np.random.multivariate_normal(mean = [0.05]*18, cov = 0.05*np.eye(18), size = 1).flatten().tolist()
+state_new = np.random.multivariate_normal(mean = [0.05]*6, cov = 0.05*np.eye(6), size = 1).flatten().tolist()
 
 state_new[0] = R * np.cos(-np.pi/2.0)
 state_new[1] = R * np.sin(-np.pi/2.0)
@@ -30,6 +31,7 @@ start= state_new[:2]
 XY = [D2D.state[:2]]
 targ = []
 
+starting_state = start
 way_point = 0
 
 for n in range(MAX_SIM_STEPS):
@@ -38,14 +40,18 @@ for n in range(MAX_SIM_STEPS):
 
     if (D2D.Cost.cost < 0.02) and n > 0:
         way_point +=1
+        starting_state = D2D.state[:2]
 
-    D2D.ym[0] = D2D.state[0] +  R * np.cos((way_point+2)/100 - np.pi/2.0) - R*np.cos(-np.pi/2.0)
-    D2D.ym[1] = D2D.state[1] +  R * np.sin((way_point+2)/100 - np.pi/2.0) - R*np.sin(-np.pi/2.0)
+    D2D.ym[0] =  R * np.cos((MAX_SIM_STEPS)/100 - np.pi/2.0)
+    D2D.ym[1] =  R * np.sin((MAX_SIM_STEPS)/100 - np.pi/2.0)
 
     state_old = state_new
+
     u_optimal_old = u_optimal
 
     D2D.state = np.array(state_new).flatten()
+
+    state_new, D2D.par_eps_par_u = D2D.predict(u = u_optimal, del_u = del_u, T = SIM_STEP)
 
     u_optimal = np.reshape(D2D_opt.optimize(u = u_optimal, del_u = del_u, rtol = 1e-8, \
                             maxit = MAX_NR_IT, verbose= False)[0], (-1, 1))
@@ -59,10 +65,7 @@ for n in range(MAX_SIM_STEPS):
 
     u_optimal = np.array(u_optimal).flatten().tolist()
 
-    state_new = D2D.predict(u = u_optimal, del_u = del_u, T = SIM_STEP)
-
     D2D.yn = state_new[:2]
-
     targ += [D2D.ym][0]
     XY += [D2D.yn]
 
@@ -71,7 +74,7 @@ XY = np.reshape(XY, (-1, 2))
 targ = np.reshape(targ, (-1, 2))
 
 labels = ["X", "Y"]
-plt.plot(XY[:, 0], XY[:, 1], '--k', label = 'position')
+plt.plot(XY[:, 0], XY[:, 1], 'ok', label = 'position')
 plt.scatter(targ[:, 0], targ[:, 1], c = 'r', marker = (5,2), label = 'target')
 plt.scatter(start[0], start[1], c = 'b', marker = (5, 2), label = 'start')
 plt.title("Position in XY Coordinates")
