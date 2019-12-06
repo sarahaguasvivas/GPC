359b12f46c0c576f35a25b5f78d4c4bc5342b181
sarahaguasvivas <sarahaguasvivas@Sarahs-MacBook-Pro.local>
2019-12-06 10:05:47 -0700

    Fixed linearization

--- python/example/car_mpc.py
+++ python/example/car_mpc.py
@@ -4,15 +4,15 @@ from optimizer.newton_raphson import *
 import matplotlib.pyplot as plt
 
 ############### TUNING PARAMS: ##########################
-MAX_ACCEL= 10.
+MAX_ACCEL= 15.
 MIN_ACCEL = -5.0
 MAX_STEERING = np.pi/4.0 - 0.2
-MAX_SIM_STEPS = 100
+MAX_SIM_STEPS = 1000
 MAX_NR_IT = 8 # Maximum Newton Raphson Iterations
 TARGET_THRESHOLD = 0.2
 #########################################################
 
-D2D = Driver2DMPC(ym = [0.0, 0.0], K = .2, yn = [0.0, 0.0], dt = 0.02)
+D2D = Driver2DMPC(ym = [0.0, 0.0], K = .5, yn = [0.0, 0.0], dt = 0.02)
 
 del_u = np.array([0.0, 0.0])
 u_optimal = np.array([10.0, 0.0])
@@ -36,15 +36,12 @@ for i in range(MAX_SIM_STEPS):
     u_optimal[1] = np.pi/4.0 * np.sin(i / D2D.dt)
 
     F, G, H, M = D2D._get_FGHM(state_new_linear, u_optimal)
-    state_new_linear = np.add(np.dot(F, state_new_linear) ,  np.dot(G, u_optimal))
+    state_new_linear = np.add(np.dot(F, state_new_ode) ,  np.dot(G, u_optimal))
 
     D2D.state = state_new_ode
     state_new_ode = D2D.predict_ode(u_optimal, [0.0, 0.0], D2D.dt)
 
     print(state_new_ode, state_new_linear)
-
-    #state_new_ode[4] = state_new_ode[4] % (np.pi/2.0)
-    #state_new_linear[4] = state_new_linear[4] % (np.pi/2.0)
 
     ode+=[state_new_ode]
     linear+=[state_new_linear]
